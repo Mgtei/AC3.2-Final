9ffbbe6e362d58ee0e013f83b36b09ce932571a2
Margaret E. Ikeda <margaretei@gmail.com>
2017-02-15 11:42:30 -0500

    started programmatic layout of logo in VC

--- AC3.2-Final/AC3.2-Final/ViewController.swift
+++ AC3.2-Final/AC3.2-Final/ViewController.swift
@@ -7,18 +7,51 @@
 //
 
 import UIKit
+import SnapKit
+import Firebase
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupViewHierarchy()
+        configureConstraints()
+        
+        //checks if user is logged in
+        if FIRAuth.auth()?.currentUser != nil {
+            dump("CURRENT USER HEREEEEEEE \(FIRAuth.auth()!.currentUser!.uid)")
+            if let uid = FIRAuth.auth()?.currentUser?.uid {
+                fetchUser(uid)
+                self.currenUserId = uid
+            }
+        }
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewWillAppear(!animated)
+        usernameTextField.setUpUnderlineLayer()
+        passwordTextField.setUpUnderlineLayer()
+    }
+    // MARK: - Setup
+    func setupViewHierarchy() {
+        self.view.addSubview(logo)
+        self.view.addSubview(usernameTextField)
+        self.view.addSubview(passwordTextField)
+        self.view.addSubview(loginButton)
+        self.view.addSubview(registerButton)
+    }
+    
+    private func configureConstraints(){
+        self.edgesForExtendedLayout = []
+        
+        // Logo
+        logo.snp.makeConstraints({ (view) in
+            view.centerX.equalTo(self.view)
+            view.width.height.equalTo(150)
+            view.top.equalToSuperview().offset(10)
+        })
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
 
 
 }
